51d1f4f549ca3d62a5fcf42627503853328ea091
allan kivuva <kivuvaallan@gmail.com>
2026-04-09 21:37:04 +0300

    Y

---
+++ script.js
@@ -0,0 +1,25 @@
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType;
+}
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
