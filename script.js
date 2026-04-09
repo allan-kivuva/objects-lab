85e34d0de583af488425fce482347f5dcaed6ac7
allan kivuva <kivuvaallan@gmail.com>
2026-04-09 21:42:51 +0300

    x

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 1. Attendee object
 const attendee = {
   attendeeId: "T001",
   name: "Alice Smith",
@@ -5,21 +6,33 @@ const attendee = {
   ticketType: "VIP",
   ticketPrice: 150.00
 };
+
+// 2. Log attendee name
 function logAttendeeName(attendee) {
   console.log(attendee.name);
 }
+
+// 3. Log ticket price
 function logTicketPrice(attendee) {
   console.log(attendee.ticketPrice);
 }
-function updateTicketType(attendee, newType) {
-  attendee.ticketType = newType;
+
+// 4. Update ticket type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
 }
-function updateTicketPrice(attendee, newPrice) {
-  attendee.ticketPrice = newPrice;
+
+// 5. Update ticket price
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
 }
+
+// 6. Remove event property
 function removeEventProperty(attendee) {
   delete attendee.event;
 }
+
+// 7. Add checkedIn property
 function addCheckedInProperty(attendee) {
   attendee.checkedIn = true;
 }
